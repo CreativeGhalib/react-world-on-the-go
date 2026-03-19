13330a8219830b48bc5ec79366bdaf3824327afa
CreativeGhalib <creativeghalib@gmail.com>
2026-03-19 21:18:20 +0600

    adding paragraph in xyz

--- src/xyz.jsx
+++ src/xyz.jsx
@@ -3,3 +3,14 @@ function Xyz() {
 }
 
 export default Xyz;
+
+function Xyz() {
+  return (
+    <div>
+      <h2>Hello React</h2>
+      <p>This is my first practice component.</p>
+    </div>
+  );
+}
+
+export default Xyz;
