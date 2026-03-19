0328483f9f1320c3c41ec0615cf6d6c6336561c8
CreativeGhalib <creativeghalib@gmail.com>
2026-03-19 21:21:14 +0600

    use of counter increment

--- src/xyz.jsx
+++ src/xyz.jsx
@@ -58,3 +58,17 @@ function Xyz() {
 }
 
 export default Xyz;
+import { useState } from "react";
+
+function Xyz() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h2>Count: {count}</h2>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  );
+}
+
+export default Xyz;
